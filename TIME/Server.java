d6a97facd77c0638027ee4b46ace506639daa99c
Giuseppe Criscione <giuseppecriscionedev@gmail.com>
2018-11-19 11:21:04 +0100

    fix Server.java

--- TIME/Server.java
+++ TIME/Server.java
@@ -1,32 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     public static void main(String[] args){
 
-        HashMap<String, Integer> books = new HashMap<>();
-
-        books.put("Fisica", 8);
-        books.put("Informatica",10);
-        books.put("Matematica", 3);
-        books.put("Latino", 1);
-        books.put("Chimica", 6);
+        List<InetAddress> blackList = new ArrayList<>();
 
         try{
-            int portNumber = 39939;
-            ServerSocket serverSocket = new ServerSocket(portNumber);
+            int portnumber = 3233;
+
+            ServerSocket serverSocket = new ServerSocket(portnumber);
 
             while(true){
-                System.out.println(">> Wait for connection on port " + portNumber + "...");
+                System.out.println(">> Wait for connection on port " + portnumber + "...");
 
                 Socket clientSocket = serverSocket.accept();
-
-                System.out.println("> Connection received from " + clientSocket.getInetAddress());
+                System.out.println("> Connection received from " + clientSocket.getInetAddress() + "(" + clientSocket.getPort() + ")");
 
                 InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
                 BufferedReader in = new BufferedReader(isr);
@@ -34,29 +27,31 @@ public class Server {
                 OutputStreamWriter osw = new OutputStreamWriter(clientSocket.getOutputStream());
                 PrintWriter out = new PrintWriter(osw, true);
 
-                String[] requests = in.readLine().split(",");
+                String request = in.readLine().trim();
 
-                if(requests.length == 2){
-                    System.out.println("> [REQUEST]  titolo_libro: " + requests[0] + ", n_copie: "+ requests[1]);
-                    if(books.containsKey(requests[0]) && (books.get(requests[0]) >= Integer.parseInt(requests[1])))
-                        out.println("True");
-                    else
-                        out.println("False");
+                if(blackList.contains(clientSocket.getInetAddress())){
+                    out.println("BANNED");
                 } else {
-                    System.out.println("> Invalid request received. Send BAD REQUEST message");
-                    out.println("[BAD REQUEST] use: <titolo_libro>,<numero_copie>");
+                    if(request.equals("TIME"))
+                        out.println(System.currentTimeMillis());
+                    else
+                        out.println("N/A");
+                    blackList.add(clientSocket.getInetAddress());
+                    System.out.println(clientSocket.getInetAddress() + " added to blacklist");
                 }
 
                 System.out.println("> Close connection");
                 clientSocket.close();
             }
 
-        } catch (Exception e){
+
+        }catch (Exception e){
             e.printStackTrace();
         }
 
-    }
 
+
+    }
 
 
 }
