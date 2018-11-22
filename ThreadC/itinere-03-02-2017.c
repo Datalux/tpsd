20a50f150728d37404875bc67a00ebcab182f728
Giuseppe Criscione <giuseppecriscionedev@gmail.com>
2018-11-22 08:43:21 +0100

    fix

--- ThreadC/itinere-03-02-2017.c
+++ ThreadC/itinere-03-02-2017.c
@@ -7,7 +7,7 @@
 int n;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void func_t0(void)
+void func_tE(void)
 {
 	int counter = 0;
 
@@ -26,17 +26,17 @@ void func_t0(void)
 		counter++;
 
 		if(counter >= 10 && (n%2 == 0)){
-			printf("[t0] termino... counter = %d, random_value = %d\n", counter, value);
+			printf("[tE] termino... counter = %d, random_value = %d\n", counter, value);
 			pthread_exit(NULL);
 		}
 		else if(counter >= 1000){
-			printf("[t0] termino... counter = %d, random_value = %d\n", counter, value);
+			printf("[tE] termino... counter = %d, random_value = %d\n", counter, value);
 			pthread_exit(NULL);
 		}
 	}
 }
 
-void func_tE(void)
+void func_tO(void)
 {
 	int counter = 0;
 
@@ -48,18 +48,18 @@ void func_tE(void)
 			value++;
 
 		pthread_mutex_lock(&mutex);
-		printf("[tE] n = %d\n",n);		
+		printf("[tO] n = %d\n",n);		
 		n += value;
 		pthread_mutex_unlock(&mutex);
 
 		counter++;
 
 		if(counter >= 10 && (n%2 != 0)){
-			printf("[tE] termino... counter = %d, random_value = %d\n", counter, value);
+			printf("[tO] termino... counter = %d, random_value = %d\n", counter, value);
 			pthread_exit(NULL);
 		}
 		else if(counter >= 1000){
-			printf("[tE] termino... counter = %d, random_value = %d\n", counter, value);
+			printf("[tO] termino... counter = %d, random_value = %d\n", counter, value);
 			pthread_exit(NULL);
 		}
 	}
@@ -71,12 +71,12 @@ int main(int argc, char **argv)
 	srand(time(NULL));
 	n = 0;
 
-	pthread_t t0, tE;
+	pthread_t tO, tE;
 
-	pthread_create(&t0, NULL, (void *) func_t0, NULL);
+	pthread_create(&tO, NULL, (void *) func_tO, NULL);
 	pthread_create(&tE, NULL, (void *) func_tE, NULL);
 
-	pthread_join(t0, NULL);
+	pthread_join(tO, NULL);
 	pthread_join(tE, NULL);
 
 	return 0;
